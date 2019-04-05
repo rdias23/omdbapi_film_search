1847570bcc631a30bb3d7b686212815e04dbc2ca
rdias23 <rdias23@gmail.com>
2019-04-05 13:16:29 -0400

    added a not found image for poster if applicable

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,7 +12,7 @@ class MoviesController < ApplicationController
              end
 
     @errors_msg = false
-    @errors_msg = "No movie match found!" if response["Response"] == "False"
+    @errors_msg = "No movie matches" if response["Response"] == "False"
 
     respond_to do |format|
       format.js { render :file => "/home/search.js.erb" }
@@ -20,11 +20,12 @@ class MoviesController < ApplicationController
   end
 
   def build_movie_attrs response
-    movie_attrs = {}
     movie_keys_to_include = [:title, :director, :year, :rated, :plot, :poster]
+    movie_attrs = response.reduce({}){ |hash,(k,v)| hash[k.downcase.to_sym] = v if movie_keys_to_include.include? k.downcase.to_sym; hash; }
 
-    response.reduce(movie_attrs){ |hash,(k,v)| hash[k.downcase.to_sym] = v if movie_keys_to_include.include? k.downcase.to_sym; hash; }
     movie_attrs[:imdb_id] = response["imdbID"]
+    movie_attrs[:poster] = "/not_found.png" if movie_attrs[:poster] == "N/A"
+
     movie_attrs["response_json"] = response.to_json
 
     movie_attrs
